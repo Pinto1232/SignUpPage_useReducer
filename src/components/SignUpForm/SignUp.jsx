609ff0fd1e2d9331ad221e2e110af0136afc15e3
Pinto Manuel <pintoydc@gmail.com>
2022-03-22 05:37:59 +0200

    Form is now designed

--- src/components/SignUpForm/SignUp.jsx
+++ src/components/SignUpForm/SignUp.jsx
@@ -2,30 +2,48 @@ import React from 'react'
 import './SignUp.css'
 
 const SignUp = () => {
+
+    const addRegisterHandler = (event) =>{
+        event.preventDefault()
+    }
   return (
       <div className="registerForm">
           <div className="RegisterContainerHeadLine">Register</div>
-          <form className="RegisterForm">
+          <form  className="RegisterForm">
               <input
                   type="text"
                   className="TextInput"
                   name="name"
                   placeholder="Name"
+                 autoComplete="new-password" 
               />
 
-                <input
-                  type="email"
-                  className="TextInput"
-                  name="email"
-                  placeholder="Email"
-              />
-
-                <input
+              <input
                   type="password"
                   className="TextInput"
                   name="password"
                   placeholder="Password"
+                  autoComplete="new-password"
               />
+
+              <input
+                  type="password"
+                  className="TextInput"
+                  name="passwordRepeat"
+                  placeholder="Password repeat"
+                 autoComplete="new-password"
+              />
+              <label className="TouchCheckboxlabel">
+                  <input
+                      type="checkbox"
+                      className="TouchCheckbox"
+                      name="termsAccepts"
+                  />
+                       Accept Terms of Use!
+              </label>
+              <button  onClick={addRegisterHandler}>
+                  Register
+              </button>
           </form>
     </div>
   )
